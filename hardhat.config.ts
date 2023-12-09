0a42743f458a2502bd51e699466e9304c8b87b9c
pradeep.nadimpalli <pradeep.nadimpalli@ivycomptech.com>
2023-12-09 22:23:04 +0530

    update: hardhat config
    Signed-off-by: pradeep.nadimpalli <pradeep.nadimpalli@ivycomptech.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    mantle: {
+      chainId: 5001,
+      accounts: [PRIVATE_KEY],
+      url: "https://rpc.testnet.mantle.xyz",
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./temp/cache",
+    artifacts: "./temp/artifacts",
+  },
+  typechain: {
+    outDir: "./temp/types",
+  },
 };
 
 export default config;
